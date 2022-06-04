542e84baa8cf222054d9defc265375584e566e7d
Natasha Zhou <Xian.Zhou72@qmail.cuny.edu>
2022-06-04 13:01:40 -0400

    get 1 to 255 using console or array

---
+++ Get 1 to 255.js
@@ -0,0 +1,17 @@
+function get_number(){
+    for (var i=1; i<256; i++){
+        console.log (i);
+    }
+}
+
+
+/*function get_array(){
+    var arr=[];
+    for (var i=0; i<255; i++){
+        arr [i]=i+1;
+    }
+    return arr;
+}
+*/
+get_number();
+//console.log(get_array());
